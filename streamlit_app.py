8c123f9cbe596c5746a260e772dfd0d32c1de306
Ellen-Benkie <134351309+Ellen-Benkie@users.noreply.github.com>
2023-06-02 23:52:52 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 &  Blueberry Oatmeal')
@@ -8,7 +12,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
@@ -22,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -30,14 +34,16 @@ streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #take the json version of the response and normalise it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it to screen as a table
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
+#import snowflake.connector
+#dont add anything here
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -48,7 +54,7 @@ streamlit.dataframe(my_data_rows)
 #new section to display fruityvice api response
 add_my_fruit = streamlit.text_input('What  would you like to add','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
 my_cur.execute ("insert into fruit_load_list values ('from streamlit')");
