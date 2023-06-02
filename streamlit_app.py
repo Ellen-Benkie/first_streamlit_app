849b6b3c4e56aaa6d8d4151f12ee3966124b4d73
Ellen-Benkie <134351309+Ellen-Benkie@users.noreply.github.com>
2023-06-03 00:26:53 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,11 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+#Create the reapeatable code block( called a function)
+def get_fruityvice_data(this_fruit_choice)
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       return fruityvice_normalized
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -37,9 +42,11 @@ try:
    if not fruit_choice:
          streamlit.error("Please select a fruit to get information")
    else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       streamlit.dataframe(fruityvice_normalized)
+       back_from_function = get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
+         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # streamlit.dataframe(fruityvice_normalized)
     
 except URLError as e:
       streamlit.error()
